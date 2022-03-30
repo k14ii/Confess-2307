8ec2c9ace72d6584b8802e11e31a9068d65adc5a
k14ii <102662636+k14ii@users.noreply.github.com>
2022-03-31 00:29:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Chào buổi tối nhoo :>",
-    introTitle: 'Hello Lọn :>',
+    introTitle: 'Hello Lọn :> Mình là chim thủy tổ hàng fake đây :>',
     introDesc: `Mình có câu này muốn hỏi Lọn lâu ời, mà hong có dám 😢 `,
     btnIntro: 'hê hê',
     title: 'Hông biết từ khi nào u mê Lọn nữa 😢 ',
@@ -9,9 +9,9 @@ const CONFIG = {
     btnNo: 'Không nha :3',
     question: 'Sao mà thoát được hehe :>',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Tại Kal vừa depzai vừa giỏi lại còn sống tìm cỏm :3',
-    mess: 'Mình giỡn thôi :v. Lọn đừng để ý. Chỉ là mình muốn nói ra những điều trong lòng thôi. Có thể Lọn vẫn chưa muốn có người yêu hoặc nhiều lý do...',
-    messDesc: 'Lọn cứ từ từ suy nghĩ nha. Tui đợi câu trả lời từ Lọn. Nếu được thì cho phép tụi mình tìm hiểu nhau cũng được ớ!',
-    btnAccept: 'Okiiiii',
+    reply: 'Tại Kal vừa depzai vừa giỏi lại còn sống tìm cỏm :3, tui cũng thích Kal lâu ời á :3',
+    mess: 'Mình giỡn thôi :v. Lọn đừng để ý. Chỉ là muốn nói ra nỗi lòng hoy 😢 Nếu có hông thích thì cũng đừng ghét tui ớ😢 . ',
+    messDesc: 'Tui biết tui hông có xứng với Lọn :v. Nhưng mà biết đâu cho tui cơ hội thì Lọn sẽ nghĩ khác thì sao :> Mình đợi câu trả lời từ Lọn nằ :))',
+    btnAccept: 'Mình đồng ý ><',
     messLink: 'https://www.facebook.com/sa.lon.5283166' 
 }
