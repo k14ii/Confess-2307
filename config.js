8c19da9addcde4af3d3939d782a7c80f32df13a6
k14ii <102662636+k14ii@users.noreply.github.com>
2022-03-30 18:29:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,1 @@
-const CONFIG = {
-    titleWeb: "Chào buổi tối Lọn nha ^^",
-    introTitle: 'Chào Lọn ^^',
-    introDesc: `Chuyện là mình có câu này muốn hỏi Lọn câu này á:((`,
-    btnIntro: '^^HiHi^^',
-    title: 'Hông biết từ khi nào mà tui đổ cậu nữa 😢 ',
-    desc: 'Lọn có thích tui hơm ớ 😢 ',
-    btnYes: 'Thích lắm :>',
-    btnNo: 'Không nha :(',
-    question: 'hí hí tớ biết mà :3. Sao thích dọ :3',
-    btnReply: 'Gửi cho bạn :>',
-    reply: 'Tại Kal vừa đẹp trai vừa giỏi ớ lại còn sống tình cỏm :3',
-    mess: 'Lọn cứ suy nghĩ đi, dù câu trả lời như thế nào thì mình cũng chấp nhận hết. ',
-    messDesc: 'Nào Lọn qua Thủ Đức mình cùng nhau đi ăn kem nha :3.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/CSyRTM/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
-}
+
