9bb942359f78d23b441425e268db5ed474be8a65
k14ii <102662636+k14ii@users.noreply.github.com>
2022-03-30 18:41:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,17 @@
-
+const CONFIG = {
+    titleWeb: "Chào buổi tối nhoo :>",
+    introTitle: 'Hello Lọn :>',
+    introDesc: `Mình có câu này muốn hỏi Lọn lâu ời, mà hong có dám 😢 `,
+    btnIntro: 'hê hê',
+    title: 'Hông biết từ khi nào u mê Lọn nữa 😢 ',
+    desc: 'Hông biết Lọn có thích tui hông á?',
+    btnYes: 'Thích lắm <3',
+    btnNo: 'Không nha :3',
+    question: 'Sao mà thoát được hehe :>',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Tại Kal vừa depzai vừa giỏi lại còn sống tìm cỏm :3',
+    mess: 'Mình giỡn thôi :v. Lọn đừng để ý. Chỉ là mình muốn nói ra những điều trong lòng thôi. Có thể Lọn vẫn chưa muốn có người yêu hoặc nhiều lý do...',
+    messDesc: 'Lọn cứ từ từ suy nghĩ nha. Tui đợi câu trả lời từ Lọn. Nếu được thì cho phép tụi mình tìm hiểu nhau cũng được ớ!',
+    btnAccept: 'Okiiiii',
+    messLink: 'https://www.facebook.com/sa.lon.5283166' 
+}
